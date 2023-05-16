27909f4a1479d154564e8b6c3070333ba52d0c65
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-16 16:53:46 -0300

    feat: completando req05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    num_set = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+
+        if num in num_set:
+            return num
+
+        num_set.add(num)
+
+    return False
