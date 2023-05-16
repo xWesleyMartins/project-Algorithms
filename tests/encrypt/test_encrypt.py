6c5f1d6fc5af7f01d319daf08a7d3b2cff2018fc
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-16 13:03:21 -0300

    feat: completando req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123, 123)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("test_message", "test_key")
+
+    assert encrypt_message("minecraft", 3) == "nim_tfarce"
+    assert encrypt_message("darksiders", 4) == "sredis_krad"
+    assert encrypt_message("bioshock", 10) == "kcohsoib"
