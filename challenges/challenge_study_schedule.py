63d6bbda7913b8db63062f24bc7587e169158703
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-16 11:43:05 -0300

    feat: concluindo req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    counter = 0
+
+    for start, end in permanence_period:
+        if not (isinstance(start, int) and isinstance(end, int)):
+            return None
+
+        if start <= target_time <= end:
+            counter += 1
+
+    return counter
